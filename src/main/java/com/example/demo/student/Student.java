b8b9778a82376ec39106816e5281ea3af1bc738e
Rytis Cepulis <cepRytis@gmail.com>
2021-09-12 14:35:57 +0300

    init, hello world things

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,49 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private String email;
+    private Integer age;
+
+    public Student(Long id ,String name, LocalDate dob, String email, Integer age) {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
