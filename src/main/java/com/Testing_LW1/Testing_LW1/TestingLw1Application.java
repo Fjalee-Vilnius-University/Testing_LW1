56186472acf9a7ed9f5e6d775c15af0e51b71c93
Rytis Cepulis <cepRytis@gmail.com>
2021-10-01 19:19:53 +0300

    was wrong spring boot, no web dependency

---
+++ src/main/java/com/Testing_LW1/Testing_LW1/TestingLw1Application.java
@@ -0,0 +1,13 @@
+package com.Testing_LW1.Testing_LW1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingLw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingLw1Application.class, args);
+	}
+
+}
