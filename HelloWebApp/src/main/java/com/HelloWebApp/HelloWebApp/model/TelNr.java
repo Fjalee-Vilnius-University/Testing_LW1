d6c4f2b052d47c318ab1befedd05405093559550
Rytis Cepulis <cepRytis@gmail.com>
2021-10-16 10:14:59 +0300

    simple model for TelNr

---
+++ HelloWebApp/src/main/java/com/HelloWebApp/HelloWebApp/model/TelNr.java
@@ -0,0 +1,39 @@
+package com.HelloWebApp.HelloWebApp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TelNr {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String telNr;
+    private int userId;
+
+    public String getTelNr() {
+        return telNr;
+    }
+
+    public void setTelNr(String telNr) {
+        this.telNr = telNr;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
