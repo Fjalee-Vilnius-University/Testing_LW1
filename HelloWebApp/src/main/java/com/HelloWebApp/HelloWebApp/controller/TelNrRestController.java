c00b43dd01084f51fa92e1462c722180e0ab3e15
Rytis Cepulis <cepRytis@gmail.com>
2021-10-24 17:51:40 +0300

    typo

--- HelloWebApp/src/main/java/com/HelloWebApp/HelloWebApp/controller/TelNrRestController.java
+++ HelloWebApp/src/main/java/com/HelloWebApp/HelloWebApp/controller/TelNrRestController.java
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 */
 
 @RestController
-public class PrekeRestController {
+public class TelNrRestController {
 	@Autowired
 	TelNrService service;
 /*
