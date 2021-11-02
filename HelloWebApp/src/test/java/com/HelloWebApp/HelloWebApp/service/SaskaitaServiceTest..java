6e1d87028d9a564b61edd7123977fece902daead
Rytis Cepulis <cepRytis@gmail.com>
2021-11-02 22:11:40 +0200

    unit tests for service classes

--- HelloWebApp/src/test/java/com/HelloWebApp/HelloWebApp/service/SaskaitaServiceTest..java
+++ HelloWebApp/src/test/java/com/HelloWebApp/HelloWebApp/service/SaskaitaServiceTest..java
@@ -1,5 +1,77 @@
 package com.HelloWebApp.HelloWebApp.service;
 
-class SaskaitaServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.HelloWebApp.HelloWebApp.model.Saskaita;
+import com.HelloWebApp.HelloWebApp.repository.SaskaitaRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SaskaitaServiceTest {
+    @Mock
+    SaskaitaRepository repository;
+
+    @InjectMocks
+    SaskaitaService service;
+
+    @Test
+    void testFindAll() {
+        var p = new Saskaita();
+        List<Saskaita> saskaitos = new ArrayList<>();
+        saskaitos.add(p);
+
+        when(repository.findAll()).thenReturn(saskaitos);
+
+        var found = (List<Saskaita>) repository.findAll();
+
+        verify(repository).findAll();
+
+        assertEquals(1, found.size());
+    }
+
+    @Test
+    void testAdd() {
+        var saskaita = new Saskaita(1, 1,1,1, "+37067676987");
+        when(repository.save(Mockito.any(Saskaita.class))).thenReturn(saskaita);
+
+        var added = service.add(saskaita);
+        verify(repository).save(Mockito.any(Saskaita.class));
+        assertNotNull(added);
+    }
+
+    @Test
+    void testUpdate() {
+        var saskaita = new Saskaita(1, 1,1,1, "+37067676987");
+        when(repository.save(Mockito.any(Saskaita.class))).thenReturn(saskaita);
+
+        service.update(saskaita);
+        verify(repository).save(Mockito.any(Saskaita.class));
+    }
+
+    @Test
+    void testDeleteById() {
+        service.deleteById(1);
+        verify(repository).deleteById(Mockito.anyInt());
+    }
+
+    @Test
+    void testFindById() {
+        Saskaita p = new Saskaita(1, 1, 1, 1, "867575865");
+        when(repository.findById(Mockito.anyInt())).thenReturn(Optional.of(p));
+
+        Saskaita found = service.findById(1);
+        verify(repository).findById(Mockito.anyInt());
+        assertNotNull(found);
+    }
 }
--- HelloWebApp/src/test/java/com/HelloWebApp/HelloWebApp/service/TelNrServiceTest..java
+++ HelloWebApp/src/test/java/com/HelloWebApp/HelloWebApp/service/TelNrServiceTest..java
@@ -1,5 +1,77 @@
 package com.HelloWebApp.HelloWebApp.service;
 
-class TelNrServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.HelloWebApp.HelloWebApp.model.TelNr;
+import com.HelloWebApp.HelloWebApp.repository.TelNrRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TelNrServiceTest {
+    @Mock
+    TelNrRepository repository;
+
+    @InjectMocks
+    TelNrService service;
+
+    @Test
+    void testFindAll() {
+        var p = new TelNr();
+        List<TelNr> telNrs = new ArrayList<>();
+        telNrs.add(p);
+
+        when(repository.findAll()).thenReturn(telNrs);
+
+        var found = (List<TelNr>) repository.findAll();
+
+        verify(repository).findAll();
+
+        assertEquals(1, found.size());
+    }
+
+    @Test
+    void testAdd() {
+        var TelNr = new TelNr("+37067676987", 1);
+        when(repository.save(Mockito.any(TelNr.class))).thenReturn(TelNr);
+
+        var added = service.add(TelNr);
+        verify(repository).save(Mockito.any(TelNr.class));
+        assertNotNull(added);
+    }
+
+    @Test
+    void testUpdate() {
+        var TelNr = new TelNr("+37067676987", 1);
+        when(repository.save(Mockito.any(TelNr.class))).thenReturn(TelNr);
+
+        service.update(TelNr);
+        verify(repository).save(Mockito.any(TelNr.class));
+    }
+
+    @Test
+    void testDeleteById() {
+        service.deleteById(1);
+        verify(repository).deleteById(Mockito.anyInt());
+    }
+
+    @Test
+    void testFindById() {
+        TelNr p = new TelNr("867575865", 1);
+        when(repository.findById(Mockito.anyInt())).thenReturn(Optional.of(p));
+
+        TelNr found = service.findById(1);
+        verify(repository).findById(Mockito.anyInt());
+        assertNotNull(found);
+    }
 }
